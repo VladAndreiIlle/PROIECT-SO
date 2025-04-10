0a9653a13eb4f63dfcb6d8febd568df4b0daf5d9
VladAndreiIlle <vlad.ille@student.upt.ro>
2025-04-10 22:25:46 +0300

    Functiile list si remove adaugate

--- treasure_manager.c
+++ treasure_manager.c
@@ -10,14 +10,14 @@
 //definim un fisier de log
 #define logfile "huntLogger"
 
-struct treasure{
+typedef struct treasure{
     int ID; //am ales tip de date int pentru a facilita simplitate si a fi mai usor de cautat
     char username[20];
     double latitude;
     double longitude;
     char clue[200];
     int value;
-};
+}Treasure;
 
 //functie pentru crearea de director, in cazul in care ele nu exista
 
@@ -127,6 +127,118 @@ int add_treasure(char *huntID, struct treasure *newTreasure){
     return 0;
 }
 
+//functie pentru a sterge comori dupa ID
+int remove_treasure(char *huntID, int treasureID){
+    char path[256];
+    sprintf(path,"%s/temp", huntID);//facem o cale (inlocuitoare)
+
+    int fd = open_treasure_file(huntID, O_RDONLY);
+    if(fd==-1) return -1;//fail
+
+    int temp = open(path, O_WRONLY|O_CREAT|O_TRUNC, 0644);
+    if(temp==-1){
+        close(fd);
+        perror("ERR: Nu s-a creat fisierul temporar(pentru inlocuirea la operatia remove)");
+        return -1; //fail
+    }
+
+    Treasure tr;
+    int found = 0;
+
+    //citim din fd original si scriem in temp toate comorile inafara de cea pentru stergere
+    while(read(fd,&tr,sizeof(Treasure))==sizeof(Treasure)){//se citeste cate un struct treasure !!
+        if(tr.ID!=treasureID){//doar daca nu e cel precizat in parametru se scrie in fisierul inlocuitor.
+            if(write(temp,&tr,sizeof(Treasure))!=sizeof(Treasure)){
+                perror("ERR: Nu s-a putut scrie in fisierul inlocuitor");
+                close(fd);
+                close(temp);
+                unlink(path);// stergere fisier temporar din file system
+                return -1;
+            }
+        }else{
+            found = 1;
+        }
+    }
+    close(fd);
+    close(temp);
+
+    if(!found){
+        unlink(path);//stergere fisier temporar din file system, de data asta la cazul in care nu e gasit
+        return -1;
+    }
+
+    char filepath[128];
+    sprintf(filepath, "%s/%s", huntID, treasurefile);
+
+    if(rename(path,filepath)==-1){//facem inlocuirea
+        perror("ERR: Nu s-a actualizat fisierul de comori");
+        return -1; //fail
+    }
+
+    char statement[256];
+    sprintf(statement, "Removed treasure %d", treasureID);
+    log_entry(huntID, statement);
+    return 0;
+}
+
+int list_treasures(char * huntID){
+    char path[256];
+    sprintf(path, "%s/%s", huntID, treasurefile);
+
+    struct stat st;
+
+    if(stat(path,&st) == -1){
+        printf("Hunt not found or treasures not added yet!\n");
+        return -1;
+    }
+
+    printf("Hunt:%s\n", huntID);
+    printf("TReasure file size: %ld B\n", st.st_size);
+
+    char time_buff[64];
+    strftime(time_buff,sizeof(time_buff), "%Y-%m-%d %H:%M:%S", localtime(&st.st_mtime));//momentul ultimei modificari
+
+    printf("Last modification took place at: %s\n", time_buff);
+
+    int fd = open(path,O_RDONLY);
+    if(fd==-1){
+        perror("ERR: Deschidere fisier comoara esuata");
+        return -1;
+    }
+
+    Treasure tr;
+    int contor = 0;
+
+    printf("Treasures:\n");
+    printf("%-5s %-20s %-20s %-10s\n", "ID","User", "Coordinates", "Value");//aliniere header tabel
+
+    printf("--------------------------------------------------------\n");
+
+    while(read(fd,&tr,sizeof(Treasure))==sizeof(Treasure)){
+        printf("%-5d %20s (%-5.6f, %-5.6f) %-10d\n",tr.ID,tr.username,tr.latitude,tr.longitude,tr.value);//aliniere tabel
+        contor++;
+    }
+
+    if(contor==0){
+        printf("Nicio comoara gasita in acest director hunt\n");
+    }else{
+        printf("Total comori: %d\n", contor);
+    }
+
+    close(fd);
+
+    char statement[64];
+    sprintf(statement, "Listed all treasures from hunt %s", huntID);
+
+    log_entry(huntID, statement);
+
+    return 0;
+}
+
+/*void instructiuni(){
+    printf("FOLOSIRE: ")
+}*/
+
 int main(){
     init_hunt_dir("Hunt1");
     creare_log_symlink("Hunt1");
@@ -134,5 +246,7 @@ int main(){
     struct treasure tr1;
     tr1.ID = 10;
     add_treasure("Hunt1", &tr1);
+    //remove_treasure("Hunt1", 10);
+    list_treasures("Hunt1");
     return 0;
 }
